b6a42466ecb5a9533d08b09572503007aa71c75d
Feioyue <liutengf521@gmail.com>
2023-02-27 17:17:47 +0800

    init

---
+++ src/main/java/com/example/feifeiblog/FeifeiblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.feifeiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeifeiblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeifeiblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/feifeiblog/FeifeiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feifeiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeifeiblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
